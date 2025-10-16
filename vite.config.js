be8d53e6ad3de66a5551648f7f815fc7daff28a5
Sanjoy Paul <sanjoy.paul@hotmail.com>
2025-10-17 00:19:53 +0600

    feat: setup project for memory game

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import '@/assets/styles/_variables.scss';`
+      }
+    }
+  }
 })
